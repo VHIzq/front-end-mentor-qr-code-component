c2249c2c46cafd6a1d56fee04297e5cbfab352f0
VHMI <vhmizq@gmail.com>
2022-02-19 14:02:08 -0600

    feat: add qr code create button

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 
 const $qrData = document.getElementById("qr-data");
 const $btnClear = document.getElementById("btn__clear");
-
+const $btnCreate = document.getElementById("btn__create");
 const qrCode = new QRCode(document.getElementById("qrcode"));
 const $qrCodeClass = document.getElementsByClassName("img__picture");
 
-$qrData.addEventListener("click", (e) => {
+$btnCreate.addEventListener("click", () => {
 
-	let dataValue = e.target.value;
+	let dataValue = $qrData.value;
 	qrCode.makeCode(dataValue);
 	
 });
